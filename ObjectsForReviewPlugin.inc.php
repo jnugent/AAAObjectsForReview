9f9c2232f8bda6c449621f4e5f8f1e62511adc54
Jason Nugent <jason.nugent@gmail.com>
2015-01-07 16:12:47 -0400

    instruction improvements

--- ObjectsForReviewPlugin.inc.php
+++ ObjectsForReviewPlugin.inc.php
@@ -438,8 +438,9 @@ class ObjectsForReviewPlugin extends GenericPlugin {
 				$ofrEADao =& DAORegistry::getDAO('ObjectForReviewEditorAssignmentDAO');
 				$assignments = $ofrEADao->getAllByUserId($user->getId());
 				if (count($assignments) == 0) {
-					$output .= '<h3>' . __('plugins.generic.objectsForReview.editor.objectsForReview') . '</h3>
-							<ul class="plain">
+					$output .= '<h3>' . __('plugins.generic.objectsForReview.editor.objectsForReview') . '</h3><p>';
+					$output .= __('plugins.generic.objectsForReview.editor.objectsForReview.clickBelow') .
+							'</p><ul class="plain">
 							<li>&#187; <a href="' . Request::url(null, 'editor', 'reviewObjectTypes') . '">' . __('plugins.generic.objectsForReview.editor.objectTypes') . '</a></li>
 							<li>&#187; <a href="' . Request::url(null, 'editor', 'objectsForReview', 'all') . '">' . __('plugins.generic.objectsForReview.editor.objectsForReview') . '</a></li>
 							<li>&#187; <a href="' . Request::url(null, 'editor', 'objectsForReviewPublishers') . '">' . __('plugins.generic.objectsForReview.manageOrganizations') . '</a></li>
